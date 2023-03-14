9b86fbcab6b71db27e69d7220758dead5ffbfe39
Flavio <f>
2023-03-14 17:45:05 -0300

    Conceito

--- RoboTupiniquim/Program.cs
+++ RoboTupiniquim/Program.cs
@@ -19,50 +19,38 @@
             String direcRobo1 = Convert.ToString(robo1[4]);
             matriz[robo1X, robo1y] = direcRobo1;
 
-            Console.WriteLine($"{robo1X} {robo1y} {matriz[robo1X, robo1y]}");
             Console.WriteLine();
             Console.WriteLine("De o comando para o robo: ");
             
             String comandoRobo1 = Console.ReadLine();
-            /*if(comandoRobo1 == "Frente")
-            {
-                String aux = matriz[robo1X, robo1y];
-                matriz[robo1X, robo1y] = null;
-                robo1X++;
-                matriz[robo1X, robo1y] = aux;
-            }
-            Console.WriteLine($"{robo1X} {robo1y} {matriz[robo1X, robo1y]} Estou aqui capitão");
-            Console.WriteLine();
-            */
             x1 = robo1X;
             y1 = robo1y;
             for (int i = 0; i < comandoRobo1.Length; i++)
             {
-                matriz[robo1X, robo1y] = null;
                 if (comandoRobo1[i].Equals("D"))
                 {
-                    switch (direcRobo1)
+                    switch (matriz[x1, y1])
                     {
-                        case "N": direcRobo1 = "L"; break;
-                        case "L": direcRobo1 = "S"; break;
-                        case "S": direcRobo1 = "O"; break;
-                        case "O": direcRobo1 = "N"; break;
+                        case "N": matriz[x1, y1] = "L"; break;
+                        case "L": matriz[x1, y1] = "S"; break;
+                        case "S": matriz[x1, y1] = "O"; break;
+                        case "O": matriz[x1, y1] = "N"; break;
 
                     }
                 }
                 else if (comandoRobo1[i].Equals("E"))
                 {
-                    switch (direcRobo1)
+                    switch (matriz[x1, y1])
                     {
-                        case "N": direcRobo1 = "O"; break;
-                        case "O": direcRobo1 = "S"; break;
-                        case "S": direcRobo1 = "L"; break;
-                        case "L": direcRobo1 = "N"; break;
+                        case "N": matriz[x1, y1] = "O"; break;
+                        case "O": matriz[x1, y1] = "S"; break;
+                        case "S": matriz[x1, y1] = "L"; break;
+                        case "L": matriz[x1, y1] = "N"; break;
 
                     }
                 }
-
-                switch (direcRobo1)
+                direcRobo1 = matriz[x1, y1];
+                switch (matriz[x1, y1])
                 {
                     case "N": y1++; break;
                     case "L": x1++; break;
@@ -71,8 +59,9 @@
                 }
 
             }
+            matriz[robo1X, robo1y] = null;
             matriz[x1, y1] = direcRobo1;
-            Console.WriteLine($"{x1} {y1} {matriz[x1,y1]}");
+            Console.WriteLine($"{x1} {y1} {matriz[x1,y1]} Estou Aqui :D");
             Console.WriteLine();
 
         }
